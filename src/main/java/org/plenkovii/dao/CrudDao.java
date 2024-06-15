7fddb07f5dfeb682ae29f58cce09833336076f0b
Ilya <ilplenkov@mail.ru>
2024-06-15 09:02:17 +0500

    cruddao added

---
+++ src/main/java/org/plenkovii/dao/CrudDao.java
@@ -0,0 +1,16 @@
+package org.plenkovii.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudDao<T, ID> {
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    T save(T entity);
+
+    Optional<T> update(T entity);
+
+    void delete(ID id);
+}
